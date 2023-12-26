2b0bc2cb0ec577660cce274c1977242a79762c9c
T34P07 <t34p07d3v@gmail.com>
2023-12-26 17:54:13 +0100

    QE

--- src/server/NPCTest.lua
+++ src/server/NPCTest.lua
@@ -2,9 +2,8 @@ local ServerScriptService = game:GetService("ServerScriptService")
 
 local Civilian = require(ServerScriptService.classes.Civilian)
 local NPC = require(ServerScriptService.classes.NPC)
-Civilian.new("Tyler", CFrame.new(-10, 0, 30))
-
-print("go")
+local civilian = Civilian.new()
+civilian:Spawn("Tyler", CFrame.new(-10, 0, 30))
 
 local NPCTest = {}
 
